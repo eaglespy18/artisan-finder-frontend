3723d8a9507503679b9c2033906c3c4a0e6928dc
mo <alhassankhidir@gmail.com>
2025-09-16 20:04:08 -0700

    Initial frontend scaffold (Vite + React + Tailwind)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import SearchResults from "./pages/SearchResults";
+import ArtisanProfile from "./pages/ArtisanProfile";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/artisan/:id" element={<ArtisanProfile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
