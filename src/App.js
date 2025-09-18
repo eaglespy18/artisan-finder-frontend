243e3242a031695ac7fb8b381d45267027bbd72c
mo <alhassankhidir@gmail.com>
2025-09-18 04:13:44 -0700

    deploy build

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import SearchResults from "./pages/SearchResults";
-import ArtisanProfile from "./pages/ArtisanProfile";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Register from "./components/pages/Register";
+import SearchResults from "./components/pages/SearchResults";
+import ArtisanProfile from "./components/pages/ArtisanProfile";
+import Navbar from "./services/Navbar";  // ✅ corrected path
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchResults />} />
---
+++ src/components/pages/Login.js
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div className="p-6 text-center">
+      <h1 className="text-2xl font-bold">Login Page</h1>
+      <p className="mt-2 text-gray-500">Here will be your login form.</p>
+    </div>
+  );
+}
